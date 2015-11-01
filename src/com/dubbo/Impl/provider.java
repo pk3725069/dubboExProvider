430f57f244f3870df336036699453109c294adb3
pk3725069 <lhzh2qh@163.com>
2015-11-01 19:37:23 +0800

    传入参数验证功能

--- src/com/dubbo/Impl/provider.java
+++ src/com/dubbo/Impl/provider.java
@@ -3,18 +3,15 @@ package com.dubbo.Impl;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.dubbo.HelloService;
+
 
 public class provider {
 
-	public static void main(String[] args) throws Exception {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[] { "provider.xml" });
-		context.start();
-
-		
-
-		System.in.read();
-	}
+	  public static void main(String[] args) throws Exception {
+//	        String config = provider.class.getPackage().getName().replace('.', '/') + "/provider.xml";
+	        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("provider.xml");
+	        context.start();
+	        System.in.read();
+	    }
 
 }
